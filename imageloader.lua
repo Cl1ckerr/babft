c7e53de369500372e0ff447a1f429c41b812b18c
Cl1cker <75802182+Cl1ckerr@users.noreply.github.com>
2022-04-25 23:49:19 -0400

    imageloader.lua

---
+++ imageloader.lua
@@ -0,0 +1,18 @@
+
+
+
+
+local Old; Old = hookfunction(syn.request, function(self)
+
+    if self.Url:find('https://imageloader.dirtgui.repl.co/GetImage') then
+
+        self.Url = 'http://75.119.130.100:8080/GetImage'
+
+    end
+
+    return Old(self)
+
+end)
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/StenDirt/Trash-Game/main/Script2.lua"))()
+
